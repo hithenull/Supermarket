29803b9ec70ce3ac0cdb5027f541ce97bc96d101
hithenull <3041315798@qq.com>
2019-12-27 11:29:40 +0800

    项目创建

---
+++ Supermarket-parents/supermarket-base/src/main/java/org/supermarket/base/App.java
@@ -0,0 +1,13 @@
+package org.supermarket.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
